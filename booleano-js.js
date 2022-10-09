e95f0d7b694c9035603eabd562249d8138941609
Rafaela Kaori Eiti <108349725+Kaori-e@users.noreply.github.com>
2022-10-09 10:05:13 -0300

    Add files via upload

---
+++ booleano-js.js
@@ -0,0 +1,25 @@
+// TEMA: Booleano numeros pares e impares
+
+const numero = 10;
+
+// Está falando que numero divisivel por 2 é par
+const numeroPar = numero % 2 === 0;
+
+if(numeroPar) {
+    console.log('o numero é par');
+} else{
+    console.log('o número é impar');
+}
+
+// O console.log está pegando o resto de cada operação, se o resto for 0 é par.
+console.log(numeroPar);
+
+
+
+
+
+
+
+
+
+
